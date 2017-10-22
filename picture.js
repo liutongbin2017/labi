23b888b2d07c4437ec5e801611b9a2e9c4a70009
liutongbin <liutongbin@gmail.com>
2017-10-22 09:04:02 +0800

    最新蜡笔照片

--- picture.js
+++ picture.js
@@ -11,15 +11,18 @@
         //         oriUrl: ""
         //     }
         // ]
-        // var pageNumber = 2;
-        // var page_count = Math.ceil(arr.length/pageNumber);
-        var current = 1;
+        var pageNumber = 2;
+        var page_count = Math.ceil(arr.length/pageNumber);
+        var pageCurrent = 1;
+        var current = 0;
+        var last = arr.length - 1;
         var $con = $(' .photo-list-view');
-        function render(current){
-            var data = arr;//arr.slice((current-1)*2,current*2);
+        function render(pageCurrent){
+            // var data = arr;
+            var data = arr.slice((pageCurrent-1)*2,pageCurrent*2);
             var str = "";
             for(var i = 0;i <= (data.length-1);i++){
-                str +="<li class='photo-view-li' index="+ i +">" +
+                str +="<li class='photo-view-li' data-idx="+ i +">" +
                     "<div class='shadow'>" +
                     "<div class='share'>分享到：" +
                     "<a class='sina-share' title='新浪微博' share-pic='http://img.labi.com/group2/M00/AE/D7/wKgCMFApt8zUFIuxAAGpWLG8nfw112=640x640.jpg'>新浪微博</a>" +
@@ -39,34 +42,39 @@
                     "</li>";
             }
 
-            console.log(str);
+            // console.log(str);
             $con.html(str);
 
         }
 
-        render(current);
+        render(pageCurrent);
 
 
+
+        function ren(index){
+
+            var $comment = $(".albums-slide-photo-content");
+
+            index = index ? index: 0; 
+            var data =arr;
+            var str = "";
+            // for{(var i = 0;i<=(data.length-1);i++)
+            str +="<li>" +
+                "<img src="+data[index]+">"+
+                "</li>";
+            // }
+            $comment.html(str);
+        }
+        // ren(index);
         $(".photo-view-li").click(function(){
             $(".albums-slide-container").css('display','block');
             $(".albums-slide-close-btn").css('z-index','145');
-            var path = $(this).attr("index");
-            var arr = ["./images/wKgCMFApt9GNPaJmAAJGWgwLhEo105=C192x192+1+1.jpg","./images/wKgCO1Apt9G5mTeSAAH0q-tDsg4281=C192x192+1+1.jpg","./images/wKgCO1Apt82sNcBeAACUY9xf8CM109=C192x192+1+1.jpg","./images/wKgCO1Apt86_HltIAALH6ZFlk2A716=C192x192+1+1.jpg","./images/wKgCO1Apt829uEDGAAHSULufwik967=C192x192+1+1.jpg"];
-            var curent = 1;
-            var $comment = $(".albums-slide-photo-content");
+            var path = $(this).data("idx");
 
-            function ren(curent){
-                var data =arr;
-                var str = "";
-                // for{(var i = 0;i<=(data.length-1);i++)
-                    str +="<li>" +
-                        "<img src="+data[path]+">"+
-                        "</li>";
-                // }
-            console.log(str);
-            $comment.html(str);
-            }
-            ren(curent);
+            current = path;
+            // var arr = ["./images/wKgCMFApt9GNPaJmAAJGWgwLhEo105=C192x192+1+1.jpg","./images/wKgCO1Apt9G5mTeSAAH0q-tDsg4281=C192x192+1+1.jpg","./images/wKgCO1Apt82sNcBeAACUY9xf8CM109=C192x192+1+1.jpg","./images/wKgCO1Apt86_HltIAALH6ZFlk2A716=C192x192+1+1.jpg","./images/wKgCO1Apt829uEDGAAHSULufwik967=C192x192+1+1.jpg"];
+            
+            ren(current);
 
             // $(".albums-slide-photo-content li").css("display","none");
 
@@ -74,11 +82,42 @@
         });
 
         $(".albums-slide-close-btn").click(function(){
-                $(".albums-slide-container").fadeToggle();
-                $(".albums-slide-close-btn").css('z-index','-10');
+            $(".albums-slide-container").fadeToggle();
+            $(".albums-slide-close-btn").css('z-index','-10');
 
-            });
-
+        });
+        $(".albums_slide_handler_left").click(function(){
+            current--;
+            if(current>=0){
+                ren(current);
+            }else{
+                return false;
+            }
+        })
+        $(".albums_slide_handler_right").click(function(){
+            current++;
+            if(current<=page_count){
+                ren(current);
+            }else{
+                return false;
+            }
+        })
+        $(".next").click(function(){
+            pageCurrent++;
+            if(pageCurrent<=page_count){
+                render(pageCurrent);
+            }else{
+                return false;
+            }
+        })
+        $(".prev").click(function(){
+            pageCurrent--;
+            if(pageCurrent>0){
+                render(pageCurrent);
+            }else{
+                return false;
+            }
+        })
 
 
 
